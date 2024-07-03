e91cd9f684cbe279ac7f48ff0472d1384ad8ff9f
Tibor Fulop <t.fulop@consultant.crif.com>
2024-07-04 00:14:55 +0200

    Adjust sample response

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,6 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		return new Response('Hello World! (Presents)');
 	},
 } satisfies ExportedHandler<Env>;
